729ee337ed9f3aa2dc92a8e6f65413e38cb2c0cc
Unknown <somluk2792@gmail.com>
2018-02-20 13:11:30 +0700

    พร้อมส่งค่ะ

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+    int cmd;
+    float a, b, sum1, sum2, sum3, sum4;
+    cout << "Welcome to CLI Calculator V1.00" << endl;
+    cout << "number 1 : ";
+    cin >> a;
+    cout << "number 2 : ";
+    cin >> b;
+
+    do{
+        cout << "Menu" << endl;
+        cout << "1. +" << endl;
+        cout << "2. -" << endl;
+        cout << "3. *" << endl;
+        cout << "4. /" << endl;
+        cout << "5. Quit" << endl;
+        cout << "Please Enter menu : ";
+        cin >> cmd;
+
+        if (cmd == 1){
+            sum1= a+b;
+            cout << "Result = " << sum1 << endl;
+        }
+        else if (cmd == 2){
+            sum2= a-b;
+            cout << "Result = " << sum2 << endl;
+        }
+        else if (cmd == 3){
+            sum3= a*b;
+            cout << "Result = " << sum3 << endl;
+        }
+        else if (cmd == 4){
+            sum4= a/b;
+            cout << "Result = " << sum4 << endl;
+        }
+        else {
+            break;
+        }
+    }while(1);
 
         return 0;
 }
--- exam2.cpp
+++ exam2.cpp
@@ -1,6 +1,72 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main() {
+        /* code */
+        int cmd;
+        string name;
+        string line;
+        ofstream write;
+        ifstream read;
+
+        do{
+            cout << "MENU" << endl;
+            cout << "1. Add student." << endl;
+            cout << "2. List student." << endl;
+            cout << "3. List student Firstname x." << endl;
+            cout << "4. find List student." << endl;
+            cout << "5. Quit." << endl;
+            cout << "Please enter menu : " ;
+            cin >> cmd;
+
+            if (cmd == 1){
+                cout << "Enter Std name : " ;
+                cin >> name;
+
+                write.open("std.txt",fstream::app);
+                if (write.is_open()) {
+                    write << name << endl;
+                    write.close();
+                } else {
+                    cout << "unable to open file";
+                }
+            }
+
+                else if (cmd == 2){
+                read.open ("std.txt");
+                if (read.is_open()) {
+                    while (getline (read,line)){
+                        cout << line << endl;
+                    }
+                }
+            }
+
+                 else if (cmd == 3){
+                read.open ("std.txt");
+                if (read.is_open()) {
+                    while (getline (read,name)){
+                        cout << name << endl;
+                    }
+                }
+            }
+
+             else if (cmd == 4){
+                read.open ("std.txt");
+                if (read.is_open()) {
+                    while (getline (read,name)){
+                        cout << name << endl;
+                    }
+                }
+            }
+
+            else {
+                break;
+            }
+
+        }while (1);
 
         return 0;
 }
+
